29c692dc7383d83de2aa0d60edeac042851f4b08
wanzicong <1360816154@qq.com>
2025-12-01 22:37:05 +0800

    add:对接api 信息

--- vite.config.ts
+++ vite.config.ts
@@ -29,7 +29,7 @@ export default defineConfig({
           cors: true,
           proxy: {
                '/api': {
-                    target: 'http://localhost:9000',
+                    target: process.env.VITE_API_BASE_URL,
                     changeOrigin: true,
                     secure: false,
                     rewrite: (path) => path.replace(/^\/api/, '/api'),
