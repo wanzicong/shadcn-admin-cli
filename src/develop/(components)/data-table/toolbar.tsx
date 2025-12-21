679dc57df08c7e5f54ac80f5691f9f9acecdd283
wanzicong <1360816154@qq.com>
2025-12-21 14:24:59 +0800

    add:修改代码逻辑

--- src/develop/(components)/data-table/toolbar.tsx
+++ src/develop/(components)/data-table/toolbar.tsx
@@ -92,7 +92,7 @@ export function DataTableToolbar<TData>({ table, searchPlaceholder = 'Filter...'
                               }}
                               className='h-8 px-2 lg:px-3'
                          >
-                              Reset
+                              重置
                               <Cross2Icon className='ms-2 h-4 w-4' />
                          </Button>
                     )}
